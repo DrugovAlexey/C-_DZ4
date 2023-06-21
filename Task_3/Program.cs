89ae2063dff9a38714c9b335a0598d500dacff49
Olga <panda146@yandex.ru>
2023-06-21 12:59:38 +0300

    added task3

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GenerateArray(int len, int min, int max)
+{
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+void PrintArrey(int[] Arrey)
+{
+    for (int i = 0; i < Arrey.Length; i++)
+    {
+        System.Console.Write(Arrey[i] + " ");
+    }
+}
+
+int[] number = GenerateArray(8, 0, 100);
+PrintArrey(number);
